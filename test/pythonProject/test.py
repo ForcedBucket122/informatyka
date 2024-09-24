9d0c7a4734e514c7e080a2fa860f325ac9ac2f8e
ForcedBucket122 <dzejkob2112@gmail.com>
2024-09-24 12:25:37 +0200

    test

--- test/pythonProject/test.py
+++ test/pythonProject/test.py
@@ -1,3 +1,13 @@
-x=4
-y=2
-print(x-y)
+from tkinter import *
+
+def press():
+    moj_tekst.config(text="Do widzenia",bg="blue")
+
+root = Tk()
+root.geometry("400x300")
+moj_tekst = Label(root,text="Dzień dobry",bg="yellow")
+moj_przycisk = Button(root,text="ok",width="200",command=press)
+moj_tekst.pack(side=RIGHT)
+moj_przycisk.pack()
+
+root.mainloop()
